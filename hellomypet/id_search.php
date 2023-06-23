9e5bedcd6740b58c8a6056dbdc6bfaf911d93da5
YunSeung <119654971+loveezis@users.noreply.github.com>
2023-06-23 14:42:14 +0900

    Add files via upload

---
+++ hellomypet/id_search.php
@@ -0,0 +1,24 @@
+<?php
+include "./dbinfo.php";
+?>
+<?php
+$mid = str_replace(" ","",$_POST['m_id']);
+$msg = "";
+
+if($mid==""){
+    $msg = "error";
+}
+else{
+    $sqls = "select * from m_member where mid='$mid'";
+    $querys = mysqli_query($connect,$sqls);
+    $rows = mysqli_num_rows($querys);
+    if(!$rows){
+        $msg = "idok";
+    }
+    else{
+        $msg = "idno";
+    }
+}
+header("Access-Control-Allow-Origin: *");
+echo urldecode($msg);
+?>
